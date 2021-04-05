3e389a8e8db1cad4237ad05c72a459af2080551e
VidhiMewada04 <76250694+VidhiMewada04@users.noreply.github.com>
2021-04-05 17:42:25 +0530

    Add files via upload
    run motor in different direction using hercules motor driver..

---
+++ motor_testing/motor_testing.ino
@@ -0,0 +1,35 @@
+#define pwm  12
+#define dir1  11
+#define dir2  10
+
+void setup() {
+  pinMode(pwm, OUTPUT);
+  pinMode(dir1, OUTPUT);
+  pinMode(dir2, OUTPUT);
+}
+
+void loop() {
+  //wheel will rotate in clockwise direction for 1 sec..
+  analogWrite(pwm, 255);               //max speed
+  digitalWrite(dir1, 1);
+  digitalWrite(dir2, 0);
+  delay(1000);            
+  
+  //wheel will rotate in anticlockwise direction for 2 sec..
+  analogWrite(pwm, 127);               //half speed
+  digitalWrite(dir1, 0);
+  digitalWrite(dir2, 1);
+  delay(2000);
+  
+  //wheel will rotate freely for 1.5 sec..
+  analogWrite(pwm, 50);
+  digitalWrite(dir1, 0);
+  digitalWrite(dir2, 0);
+  delay(1500);
+  
+  //wheel will stop rotating for 5 sec..
+  analogWrite(pwm, 0);
+  digitalWrite(dir1, 1);
+  digitalWrite(dir2, 1);
+  delay(5000);
+}
