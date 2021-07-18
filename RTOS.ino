1e3baa220a40a12b7659a8b1358553c49233afa2
VidhiMewada04 <76250694+VidhiMewada04@users.noreply.github.com>
2021-07-18 21:18:49 +0530

    Add files via upload

---
+++ RTOS.ino
@@ -0,0 +1,31 @@
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;
+#else
+static const BaseType_t app_cpu = 1;
+#endif
+static const int led_pin = LED_BUILTIN;
+
+void toggleLED(void *parameter){
+  while(1){
+    digitalWrite(led_pin, HIGH);
+    vTaskDelay(500/ portTICK_PERIOD_MS);
+    digitalWrite(led_pin, LOW);
+    vTaskDelay(500/ portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+  xTaskCreatePinnedToCore(
+              toggleLED,
+              "Toggle LED",
+              1024,
+              NULL,
+              1, 
+              NULL,
+              app_cpu );
+}
+
+void loop() {
+  
+}
